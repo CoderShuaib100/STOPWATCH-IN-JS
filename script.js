ac8b5e8269134123b2609c6fdf6ca012a76fbaad
CoderShuaib100 <sayeedishuaib@gmail.com>
2024-10-08 20:57:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// creating a function for the stopwatch
+
+window.onload = function() {
+    var sec = 00;
+    var ms = 00;
+    var appendS = document.getElementById("s");
+    var appendMS = document.getElementById("ms");
+    var btnStart = document.getElementById("start");
+    var btnStop = document.getElementById("stop");
+    var btnReset =document.getElementById("reset");
+    var Interval;
+}
